24c0b5cdf7be277f9b73962e7c59039c60393c38
Sapana Kale <44019343+sapanaKale@users.noreply.github.com>
2018-10-31 10:57:12 +0530

    testCases added  run_all_test file for createPattern.

--- scripts/run_all_test.sh
+++ scripts/run_all_test.sh
@@ -3,3 +3,4 @@ set -e
 ./scripts/run_test.sh ./createRectangle.js ./appTestData/rectangle_input ./appTestData/rectangle_output
 ./scripts/run_test.sh ./createTriangle.js ./appTestData/triangle_input ./appTestData/triangle_output
 ./scripts/run_test.sh ./createDiamond.js ./appTestData/diamond_input ./appTestData/diamond_output
+./scripts/run_test.sh ./createPattern.js ./appTestData/pattern_input ./appTestData/pattern_output
